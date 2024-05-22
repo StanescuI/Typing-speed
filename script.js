f020e23b4aab3eea8ac298c59d6b322483308b82
StanescuI <162870985+StanescuI@users.noreply.github.com>
2024-05-22 15:57:53 +0300

    cleaner code
    Removed magic numbers
    Restructured lines with over 80 columns into two separate lines

--- script.js
+++ script.js
@@ -1,88 +1,89 @@
+const MILLISECONDS_INCREASE = 1000 / 60
 let wordDisplay = document.getElementById('text')
 let timeDisplay = document.getElementById('time')
 let result = document.getElementById('result')
-let words = ['these', 'are', 'some', 'regular', 'words', 'that', 'coming', 'to', 'my', 'mind', 'in', 'no', 
-            'particular', 'order', 'and', 'i', 'will', 'stop', 'when', 'reached', 'the', 'end', 'of', 'line',
-            'okay', 'alright', 'awesome', 'amazing', 'extraordinary', 'cool', 'shiny', 'fresh', 'gaming', 'cat',
-            'dog', 'banana', 'pizza', 'elephant', 'you', 'me', 'him', 'her', 'they', 'them', 'wellcode', 'software']
-let randomPicker = -1
-let selectedWord
+let words = ['these', 'are', 'some', 'regular', 'words', 'that', 'coming',
+             'to', 'my', 'mind', 'in', 'no', 
+            'particular', 'order', 'and', 'i', 'will', 'stop', 'when',
+            'reached', 'the', 'end', 'of', 'line',
+            'okay', 'alright', 'awesome', 'amazing', 'extraordinary',
+            'cool', 'shiny', 'fresh', 'gaming', 'cat',
+            'dog', 'banana', 'pizza', 'elephant', 'you', 'me', 'him',
+            'her', 'they', 'them', 'wellcode', 'software']
 let letters
-let i = 0
 document.addEventListener('keydown', checkLetter)
-let accuracy
-let noWords
-let noLetters
 let timeId
-let rightLetters = 0
-let seconds
-let mili
 let inTyping = false
+let letterIndex
+let noLetters
+let noWords
+let rightLetters
 
 function startTyping() {
-    newWord()
-    inTyping = true
+    let randomPicker = -1;
+    milli = 0
+    seconds = 0
+    rightLetters = 0
     noWords = 0
+    inTyping = true
     noLetters = 0
     rightLetters = 0
-    seconds = 00
-    mili = 00
-    timeId = setInterval(increaseTime, 1000 / 60)
-    result.textContent = "TYPING SPEED . Press SPACE to reset"
-    let startButton = document.getElementById('startButton')
-    startButton.innerText = 'Reset'
-    startButton.setAttribute('onclick', 'location.reload()');
+    newWord(randomPicker)
+    timeId = setInterval(increaseTime(milli, seconds), MILLISECONDS_INCREASE);
+    result.textContent = 'TYPING SPEED . Press SPACE to reset'
 }
 
-function increaseTime() {
-    ++mili
-    if (mili == 60) {
+function increaseTime(milli, seconds) {
+    ++milli
+    if (milli == 60) {
         ++seconds
-        mili = 0
+        milli = 0
     }
     if (seconds == 60) {
         stopTyping()
     }
-    timeDisplay.textContent = "Time : " + seconds.toString().padStart(2, '0') + ':' + mili.toString().padStart(2, '0');
+    timeDisplay.textContent = 'Time : ' + seconds.toString().padStart(2, '0') +
+                              ':' + milli.toString().padStart(2, '0');
 }
 
-function stopTyping() {
+function stopTyping(noWords) {
     clearInterval(timeId)
     inTyping = false
-    result.textContent = `Congratulations , your have managed to type ${noWords} words in this minute,
-                         managing to score ${rightLetters} letters out of a total of ${noLetters}!\nYour
-                        accuracy is ${(rightLetters / noLetters * 100).toFixed(2)}%`
+    result.textContent = `Congratulations , your have managed to type ` +
+                         `${noWords} words, managing to` +
+                         ` score ${rightLetters} letters out of a total` +
+                         ` of ${noLetters}!\nYour accuracy is ` +
+                         `${(rightLetters / noLetters * 100).toFixed(2)}%`
 }
 
 function checkLetter(e) {
     if (e.key == ' ' && !inTyping) {
         startTyping()
-    } else if (e.key == ' ' && inTyping) {
-        location.reload()
-    } else {
-        let currLetter = wordDisplay.childNodes[i]
-        if (e.key == letters[i]) {
+    } else if (e.key == ' ') {
+        stopTyping(noWords)
+    } else if (inTyping && e.key != ' ') {
+        let currLetter = wordDisplay.childNodes[letterIndex]
+        if (e.key == letters[letterIndex]) {
             currLetter.classList.add('rightLetter')
             ++rightLetters
         } else {
             currLetter.classList.add('wrongLetter')
         }
-        ++i
-        if (i == letters.length) {
+        ++letterIndex
+        if (letterIndex == letters.length) {
             newWord()
-            i = 0
         }
         ++noLetters
     }
 }
 
-function newWord() {
-    i = 0
+function newWord(randomPicker) {
+    letterIndex = 0
     oldPick = randomPicker
     while (oldPick == randomPicker) {
         randomPicker = Math.floor(Math.random() * words.length)
     }
-    selectedWord = words[randomPicker]
+    let selectedWord = words[randomPicker]
     letters = selectedWord.split('')
     wordDisplay.textContent = ''
     letters.forEach(letter => {
